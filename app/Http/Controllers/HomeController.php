abd766fb85f422e8a54545dad8347a77b71d7322
victor santos <vsantos@zippz.com>
2022-07-21 20:35:46 +0800

    after login redirect to commenting page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return redirect('/id/1');
+        //return view('home');
+
     }
 }
